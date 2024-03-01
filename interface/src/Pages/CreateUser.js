4da0c4228faef5f5590854310bee9f246423d0cd
LT69018 <jturner3@umbc.edu>
2024-02-29 21:17:46 -0500

    Begin adding backend testing instructions.
    Since I'm probably not going to have time to create form elements to get the user's arguments and pass them to the backend for them, will try to let the Grader see how I expect the backend to be called.

--- interface/src/Pages/CreateUser.js
+++ interface/src/Pages/CreateUser.js
@@ -3,14 +3,24 @@ import React, {Component} from 'react';
 
 
 class CreateUser extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
 
   render() {
+    const demo_post_create_body = "{'new_user':{'id':50, 'name':'Allgood', 'points':100 }}"
+
     return (
       <div classname="CreateUser" style={{height:"15vh", marginBottom:"5vh"}}>
-        Create users here!
+        Ideally you'd be able to create users here!
+        But, I haven't had time to implement front end support for this.
+
+        <div>
+        Feel free to use CURL or postman.
+        i.e. <div style={{color: "green"}}>POST 127.0.0.1:5000/create</div>
+        Use whatever IP address / localhost name is equivalent on your machine.
+        </div>
+        <hr/>
+        Make sure to set 'Content-type':'application/json'
+        Send the new_user data in the <span style={{color: "blue"}}>'body'</span> of the request.
+        i.e. <div style={{color: "green"}}>{demo_post_create_body}</div>
       </div>
     );
   }
